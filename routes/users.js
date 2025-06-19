9193c5b3f01aa3f144178267340756e447dac5d1
cmellor00 <connormellor68@gmail.com>
2025-06-19 16:28:54 -0600

    added /me and /all routes for admin functionality

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,53 @@
 import express from "express";
 const router = express.Router();
 
-
+import db from "../db/client.js";
 import { createUser, getUserByUsernameAndPassword } from "../db/queries/users.js";
 import requireBody from "../middleware/requireBody.js";
+import requireUser from "../middleware/requireUser.js";
+import requireAdmin from "../middleware/requireAdmin.js";
 import { createToken } from "../utils/jwt.js";
 
-
-router
-    .route("/register")
-    .post(requireBody(["username", "password"]), async (req, res, next) => {
-        try {
-            const { username, password } = req.body;
-            const user = await createUser(username, password);
-            const token = createToken({ id: user.id });
-            res.status(201).json({ token });
-        } catch (err) {
-            if (err.code === "23505") {
-                res.status(400).json({ error: "Username already exists." });
-            } else {
-                next(err);
-            }
-        }
-    });
-
-
-router
-    .route("/login")
-    .post(requireBody(["username", "password"]), async (req, res) => {
+// REGISTER
+router.post("/register", requireBody(["username", "password"]), async (req, res, next) => {
+    try {
         const { username, password } = req.body;
-        const user = await getUserByUsernameAndPassword(username, password);
-        if (!user) return res.status(401).send("Invalid username or password.");
+        const user = await createUser(username, password);
+        const token = createToken({ id: user.id, is_admin: user.is_admin });
+        res.status(201).json({ token });
+    } catch (err) {
+        if (err.code === "23505") {
+            res.status(400).json({ error: "Username already exists." });
+        } else {
+            next(err);
+        }
+    }
+});
 
-        const token = createToken({ id: user.id });
-        res.json({ token });
-    });
+// LOGIN
+router.post("/login", requireBody(["username", "password"]), async (req, res) => {
+    const { username, password } = req.body;
+    const user = await getUserByUsernameAndPassword(username, password);
+    if (!user) return res.status(401).send("Invalid username or password.");
+
+    const token = createToken({ id: user.id, is_admin: user.is_admin });
+    res.json({ token });
+});
+
+// GET /users/me - Return logged-in user's profile
+router.get("/me", requireUser, async (req, res) => {
+    const { id } = req.user;
+    const {
+        rows: [user],
+    } = await db.query("SELECT id, username, is_admin FROM users WHERE id = $1", [id]);
+
+    res.json(user);
+});
+
+// GET /users/all - Admin only
+router.get("/all", requireUser, requireAdmin, async (req, res) => {
+    const { rows } = await db.query("SELECT id, username, is_admin FROM users");
+    res.json(rows);
+});
 
 export default router;
